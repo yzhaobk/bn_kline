9b1a1d8d6f2cbf61f3bad23e5c149927d1dcd58c
yzhaobk <yzhaobk@gmail.com>
2024-11-16 13:44:00 +0800

    update

--- load_data.py
+++ load_data.py
@@ -4,6 +4,22 @@ from downloader import get_path
 
 
 def load_binance_kline(symbol: str, freq: str, date: str) -> pd.DataFrame:
+    """
+    从本地文件加载 Binance K 线数据
+    :param symbol:
+    :param freq:
+    :param date:
+    :return:
+
+    symbol: BTCUDST 交易对符号
+    base: BTC
+    quote: USDT
+
+    volume: 交易量 按照 base 计量
+    quote_asset_volume: 交易量 按照 quote 计量
+    buy_base_volume: 买入量 按照 base 计量
+    buy_quote_volume: 买入量 按照 quote 计量
+    """
     path = get_path(symbol, freq, date)
 
     # 定义小写列名
@@ -25,6 +41,29 @@ def load_binance_kline(symbol: str, freq: str, date: str) -> pd.DataFrame:
     df["sell_quote_volume"] = df["quote_asset_volume"] - df["buy_quote_volume"]
 
     return df
+
+
+def read_binance_kiline(symbol: str, freq: str, date: str) -> pd.DataFrame:
+    path = get_path(symbol, freq, date)
+    if not path.exists():
+        downloader.download_binance_kline(symbol, freq, date)
+    df = load_binance_kline(symbol, freq, date)
+    return df
+
+
+def read_kline_range(symbol, freq, start_date, end_date):
+    """
+    读取一段时间的 K 线数据
+    """
+    date = start_date
+    dfs = []
+    while date <= end_date:
+        print(f"读取 {date} 的数据")
+        df = read_binance_kiline(symbol, freq, date)
+        dfs.append(df)
+        date = (pd.to_datetime(date) + pd.Timedelta(days=1)).strftime("%Y-%m-%d")
+
+    return pd.concat(dfs, ignore_index=True)
 
 
 if __name__ == "__main__":
